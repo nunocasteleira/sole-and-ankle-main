49fbb3b2056f979d54c295977d3f07facef4b833
Nuno Casteleira <nunojoaocas@gmail.com>
2021-07-23 12:46:12 +0100

    first commit

---
+++ src/components/ShoeGrid/ShoeGrid.js
@@ -0,0 +1,31 @@
+import React from "react";
+import styled from "styled-components/macro";
+
+import SHOES from "../../data";
+import ShoeCard from "../ShoeCard";
+
+const ShoeGrid = () => {
+  return (
+    <Wrapper>
+      {SHOES.map((shoe) => (
+        <ShoeWrapper key={shoe.slug}>
+          <ShoeCard {...shoe} />
+        </ShoeWrapper>
+      ))}
+    </Wrapper>
+  );
+};
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  gap: 32px;
+  /* margin: -16px; */
+`;
+
+const ShoeWrapper = styled.div`
+  flex: 1 0 275px;
+  /* margin: 16px; */
+`;
+
+export default ShoeGrid;
